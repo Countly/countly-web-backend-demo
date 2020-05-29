2715eace79995f43e68e991679302ee938c2b1c4
Cem Karakurt <ickarakurt@gmail.com>
2020-05-30 00:30:23 +0300

    missing parante

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set('view engine', 'jade');
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.app.use(express.static(path.join(__dirname, 'public'))));
 (__dirname + '/public'));
 
 app.options('*', cors())
